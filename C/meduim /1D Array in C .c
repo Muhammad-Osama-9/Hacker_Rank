b5a837aae304c12154194abf823f76cf1579d3d4
Muhammad Osama Elaraby <112892754+Muhammad-Osama-9@users.noreply.github.com>
2023-08-13 18:37:15 +0300

    Update 1D Array in C .c

--- C/meduim /1D Array in C .c
+++ C/meduim /1D Array in C .c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int size , result=0 ;
+     scanf("%d",&size);
+    int arr [size] ;
+    for (int i =0 ; i<size ;i++)
+    {
+        scanf("%d",&arr[i]);
+        result = result +  arr[i] ; 
+     }
+     printf("%d",result);
+    return 0;
+}
