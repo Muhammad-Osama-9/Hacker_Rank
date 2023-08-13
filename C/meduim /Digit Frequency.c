4804162d5943bb1edc49a56efbf0b940a9a5bd9c
Muhammad Osama Elaraby <112892754+Muhammad-Osama-9@users.noreply.github.com>
2023-08-13 18:46:42 +0300

    Update Digit Frequency.c

--- C/meduim /Digit Frequency.c
+++ C/meduim /Digit Frequency.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+   int arr [10] = {0} ;
+   char * ptr ='\0';
+  ptr = (char*)malloc(1000*sizeof(char)) ;
+
+  scanf("%s",ptr);
+
+  ptr = realloc(ptr,(strlen(ptr)+1));
+
+  for(int j=0;j<10; j++)
+   {
+         for (int i = 0 ;i<strlen(ptr) ; i++)
+      {
+            if (ptr[i]==j+48)
+                  arr[j]++;
+      }
+
+   }
+
+    for(int k = 0 ; k <10 ; k++)
+    {
+       printf("%d ",arr[k]);
+    }
+    return 0;
+}
 
